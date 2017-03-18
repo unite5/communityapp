2667fde26fa1ef3cc8cf03b84c10a3f1b9aa5962
Jitendra Patwa <jitendrapatwa135@gmail.com>
2017-03-19 01:13:28 +0530

    first

--- src/pages/myprofile/myprofile.ts
+++ src/pages/myprofile/myprofile.ts
@@ -1,38 +1,56 @@
 import { Component } from '@angular/core';
 
 import { NavController, NavParams } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular';
+import { ViewController } from 'ionic-angular';
+
+import { Tabhome } from '../tabhome/tabhome';
+
 
 @Component({
   selector: 'page-myprofile',
   templateUrl: 'myprofile.html'
 })
 export class Myprofile {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams,public actionSheetCtrl: ActionSheetController,public viewCtrl:ViewController) {
+    
   }
 
   itemTapped(event, item) {
     // That's right, we're pushing to ourselves!
-    this.navCtrl.push(Myprofile, {
-      item: item
+  }
+  
+  changeImageToCall(){
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Upload Image For The Post',
+      buttons: [
+        {
+          text: 'Gallery',
+          handler: () => {
+            console.log('gallery clicked');
+          }
+        },{
+          text: 'Camera',
+          handler: () => {
+            console.log('camera clicked');
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  showHomePage(){
+    //this.viewCtrl.dismiss();
+    this.navCtrl.setRoot(Tabhome,{
+      'message':'home'
     });
   }
 }
